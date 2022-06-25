a08207aa81dbaa5d4fdf3fead5e9fc68298f52fc
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-25 16:43:49 +0530

    [Biplab] Add: Checking time for inserting details in AddressBook

--- AddressBook_Multithreading/AddressBookOperation.cs
+++ AddressBook_Multithreading/AddressBookOperation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AddressBook_Multithreading
 {
@@ -18,10 +19,24 @@ namespace AddressBook_Multithreading
             );
             Console.WriteLine(this.Addresslist.ToString());
         }
+        public void AddContactsThread(List<AddressBook> Addresslist)
+        {
+            Addresslist.ForEach(AddData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Console.WriteLine("Data being added: " + AddData.FirstName + " " + AddData.LastName + " " + AddData.Address + " " + AddData.City + " " + AddData.State + " " + AddData.Zip + " " + AddData.PhoneNumber + " " + AddData.Email);
+                    this.AddDetails(AddData);
+                    Console.WriteLine("Contact added: " + AddData.FirstName + " " + AddData.LastName + " " + AddData.Address + " " + AddData.City + " " + AddData.State + " " + AddData.Zip + " " + AddData.PhoneNumber + " " + AddData.Email);
+
+                });
+                thread.Start();
+            });
+            Console.WriteLine(this.Addresslist.Count);
+        }
         public void AddDetails(AddressBook address)
         {
             Addresslist.Add(address);
         }
     }
 }
-
